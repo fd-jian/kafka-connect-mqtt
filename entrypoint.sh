52d53eb332af1e6863afba6ac920567c4a511dc6
Felix Jian <felix.d.jian@gmail.com>
2020-06-29 12:18:31 +0200

    improve entrypoint output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-mkdir -p /jars && cp /project/build/libs/*.jar /jars
+echo running entrypoint
+echo copying jar files &&
+    mkdir -p /jars &&
+    cp /project/build/libs/*.jar /jars &&
+    echo jar files copied successfully
+    #tail -f /dev/null
